f3f7de3c3f4f2b486719d5dfbea4cc0068a67c08
Qulu Quliyev <quluquliyevcv@gmail.com>
2023-07-16 21:36:18 +0400

    Project Finished

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def index(request):
+    posts = Blog.objects.all()
+    paginator = Paginator(posts, 5)  # Her sayfada 5 nesne
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context={
+        'posts':posts,
+        'page_obj':page_obj
+    }
+    if request.method == 'POST':
+        text_value = request.POST.get('text')
+        blog = Blog(text=text_value)
+        blog.save()
+        return render(request, 'index.html',context)
+
+    return render(request, 'index.html',context)
